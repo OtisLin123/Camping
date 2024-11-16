eb78baa8acfbec046458ce9b459c2f770b3ff823
OtisLin <otislin1102@gmail.com>
2024-11-16 22:00:13 +0800

    修正usecase 使用.

--- lib/feature/camping_list/presentation/camping_list_page.dart
+++ lib/feature/camping_list/presentation/camping_list_page.dart
@@ -1,5 +1,4 @@
 import 'package:camping/core/data/model/camping_site.dart';
-import 'package:camping/core/domain/usecase/imp_get_camping_site_use_case.dart';
 import 'package:camping/core/domain/usecase/mock_get_camping_site_use_case.dart';
 import 'package:camping/extension/string_extension.dart';
 import 'package:camping/feature/camping_list/presentation/camping_card.dart';
@@ -29,7 +28,7 @@ class _CampingListPageState extends State<CampingListPage>
   void initState() {
     super.initState();
     controller = CampingListController(
-      getCampingSiteUseCase: ImpGetCampingSiteUseCase(),
+      getCampingSiteUseCase: MockGetCampingSiteUseCase(),
       protocol: this,
     );
     controller?.fetchCampingSities();
